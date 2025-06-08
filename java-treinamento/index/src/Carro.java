65df3e4ffaba4c7f6b97760d0db9b4d6e81df8bd
Pedro <pedrovlimaa00@gmail.com>
2025-06-07 22:52:10 -0300

    Adicionei e modifiquei um arquivo java

---
+++ java-treinamento/index/src/Carro.java
@@ -0,0 +1,8 @@
+public class Carro {
+    public static void main(String[] args) throws Exception {
+        String carro = "BMW";
+
+        System.out.println("O meu carro que comprei foi uma " + carro );
+
+    }
+}
