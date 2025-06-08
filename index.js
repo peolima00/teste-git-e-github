ab3027ec7a193fef9aaa7e90374a50a663e24cf8
Pedro <pedrovlimaa00@gmail.com>
2025-06-07 23:58:25 -0300

    Adicionei uma nova variável

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let pokemon = "charizard";
 let pokemon1 = "Pikachu"
-
+let pokemon2 = "caterpie"
 console.log(`O meu porkemon favorito ${pokemon} está no level 25. Agora o mais forte é o ${pokemon1}.`)
